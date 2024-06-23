17394059bfd08aa5ea3426f2aaba11ba486235d6
Yohannes Mesay <johnnymesay@gmail.com>
2024-06-23 18:28:38 +0300

    ui update

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -6,18 +6,18 @@ function SearchOrder() {
   const navigate = useNavigate();
   function handleSubmit(e) {
     e.preventDefault();
-    if(!query) return ;
+    if (!query) return;
     navigate(`/order/${query}`);
-    setQuery('');
+    setQuery("");
   }
   return (
     <form onSubmit={handleSubmit}>
       <input
-        type="text" 
+        type="text"
         placeholder="Search the order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="rounded-full bg-yellow-100 px-4 py-2 w-28 sm:w-64 placeholder:text-stone-400 focus:outline-none"
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );

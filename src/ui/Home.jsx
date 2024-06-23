4d588b33a07a5c687897e7133a6fb7e03ddddbcb
Yohannes Mesay <johnnymesay@gmail.com>
2024-06-23 06:41:53 +0300

    update

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,11 +2,13 @@ function Home() {
   return (
     <div>
       <h1 className="text-xl text-yellow-300">
-        The best pizza.
+        <h1 className="text-yellow-300  underline">Hello world!</h1>
         <br />
         Straight out of the oven, straight to you.
       </h1>
     </div>
+
+    
   );
 }
 

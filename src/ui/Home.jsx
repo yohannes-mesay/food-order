3af328e3869b86f8175a25bcfcce3d85559b7473
Yohannes Mesay <johnnymesay@gmail.com>
2024-06-23 06:36:26 +0300

    tailwind

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="text-xl text-yellow-300">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.

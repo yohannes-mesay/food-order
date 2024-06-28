9fd0355871c4b4a17f331570b9b4bcb48a63072a
Yohannes Mesay <johnnymesay@gmail.com>
2024-06-28 16:28:47 +0300

    setting priority for orders

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Home from './ui/Home';
-import Error from './ui/Error';
-import Menu, { loader as menuLoader } from './features/menu/Menu';
-import Cart from './features/cart/Cart';
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
 import CreateOrder, {
   action as createOrderAction,
-} from './features/order/CreateOrder';
-import Order, { loader as orderLoader } from './features/order/Order';
-import AppLayout from './ui/AppLayout';
-
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import { action as updatePriority } from "./features/order/UpdateOrder";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
@@ -17,26 +17,27 @@ const router = createBrowserRouter([
 
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/menu',
+        path: "/menu",
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
-      { path: '/cart', element: <Cart /> },
+      { path: "/cart", element: <Cart /> },
       {
-        path: '/order/new',
+        path: "/order/new",
         element: <CreateOrder />,
         action: createOrderAction,
       },
       {
-        path: '/order/:orderId',
+        path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updatePriority,
       },
     ],
   },

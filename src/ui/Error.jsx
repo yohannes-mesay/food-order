ce0c4e8654eb68dc8a78e49120ee69c36b585eda
Yohannes Mesay <johnnymesay@gmail.com>
2024-06-30 20:50:24 +0300

    finilize

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -7,7 +7,7 @@ function Error() {
 
   return (
     <div>
-      <h1>Something went wrong 😢</h1>
+      <h1>Somthing went wrong</h1>
       <p>{error.data || error.message}</p>
 
       <LinkButton to="-1">&larr; Go back</LinkButton>

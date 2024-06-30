06ab9bd0a51736651d5dd06029eaecea465b2c72
Yohannes Mesay <johnnymesay@gmail.com>
2024-06-30 18:10:06 +0300

    finilize

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,12 +6,10 @@ function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
-        Fast React Pizza
+        Fast Pizza
       </Link>
-
       <SearchOrder />
       <Username />
-
     </header>
   );
 }
